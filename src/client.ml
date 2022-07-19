1e3d79c546ee8a2c428dc3532c5c5adae9bb4941
Pablo Donato <pablo.donato@tuta.io>
2022-07-19 18:27:28 +0200

    Error handling

--- src/client.ml
+++ src/client.ml
@@ -9,6 +9,10 @@ let make_req (cmd : string) (param : string) =
   let uri = Uri.of_string (addr ^ "/" ^ cmd) in
   Client.post ~body uri
 
+exception ActemaError of string
+exception UnsupportedRequestMethod of string
+exception UnsupportedHttpResponseCode of int
+
 let action (goal : Logic_t.goal) : Logic_t.atree t =
   (** Send request with goal *)
 
@@ -22,13 +26,17 @@ let action (goal : Logic_t.goal) : Logic_t.atree t =
 
   (** Receive response with action tree *)
 
-  (* let code = resp |> Response.status |> Code.code_of_status in
-  Printf.printf "Response code: %d\n" code;
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string); *)
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
 
-  body |> Cohttp_lwt.Body.to_string >|= fun proofb ->
-  (* Printf.printf "Body of length: %d\n" (String.length body); *)
-  
-  proofb |>
-  Base64.decode_exn |>
-  Logic_b.atree_of_string
+  let code = resp |> Response.status |> Code.code_of_status in
+  match code with
+  | 200 ->
+      body |>
+      Base64.decode_exn |>
+      Logic_b.atree_of_string
+  | 501 ->
+      raise (UnsupportedRequestMethod body)
+  | 550 ->
+      raise (ActemaError body)
+  | _ ->
+      raise (UnsupportedHttpResponseCode code)
--- theories/Test.v
+++ theories/Test.v
@@ -2,9 +2,5 @@ From Actema Require Import Loader.
 
 Lemma test (A B : Prop) : A -> B -> A -> B -> A.
 Proof.
-  actema;
-  actema;
-  actema;
-  actema;
-  assumption.
+  intros; assumption.
 Qed.
