68b8832103c5599101984c25e96723e54e94f33a
Pablo Donato <pablo.donato@tuta.io>
2022-07-24 16:59:39 +0200

    Add some tautos

--- theories/Tautos.v
+++ theories/Tautos.v
@@ -2,12 +2,61 @@ From Actema Require Import Loader.
 
 Parameters (A B C D E : Prop).
 
+(** * Implication *)
+
 Lemma id : A -> A.
 Proof.
   actema.
 Qed.
 
 Lemma K_combinator : A -> B -> A.
+Proof.
+  actema.
+Qed.
+
+Lemma swap_args : (A -> B -> C) -> B -> A -> C.
+Proof.
+  actema.
+Qed.
+
+Lemma compose : (A -> B) -> (B -> C) -> A -> C.
+Proof.
+  actema.
+Qed.
+
+(** * Conjunction *)
+
+Lemma and_comm : A /\ B -> B /\ A.
+Proof.
+  actema.
+Qed.
+
+Lemma and_intro : A -> B -> A /\ B.
+Proof.
+  actema.
+Qed.
+
+Lemma and_elim_l : A /\ B -> A.
+Proof.
+  actema.
+Qed.
+
+Lemma and_elim_r : A /\ B -> B.
+Proof.
+  actema.
+Qed.
+
+Lemma currying : (A /\ B -> C) -> A -> B -> C.
+Proof.
+  actema.
+Qed.
+
+Lemma uncurrying : (A -> B -> C) -> (A /\ B -> C).
+Proof.
+  actema.
+Qed.
+
+Lemma and_imp_weak_distr : (A -> B) /\ C -> A -> B /\ C.
 Proof.
   actema.
 Qed.
