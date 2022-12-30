17ea8a975e83937541ab64a90e3f5b34b760b211
Pablo Donato <pablo.donato@tuta.io>
2022-12-30 14:13:12 +0100

    Use node 16.x on Nix + Update prover

--- shell.nix
+++ shell.nix
@@ -2,10 +2,11 @@ with import <nixpkgs> {};
 
 mkShell {
     nativeBuildInputs = [
-        nodejs
-        yarn
+        nodejs-16_x
+        (yarn.override { nodejs = nodejs-16_x; })
         electron
     ];
 
     ELECTRON_OVERRIDE_DIST_PATH = "${electron}/bin";
+    NODE_OPTIONS = "--openssl-legacy-provider";
 }
