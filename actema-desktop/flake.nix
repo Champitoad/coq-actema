03b5071db287ec73a6b970854e2083014e397282
Pablo Donato <pablo.donato@tuta.io>
2024-02-29 14:24:36 +0100

    Update nix config

--- actema-desktop/flake.nix
+++ actema-desktop/flake.nix
@@ -6,5 +6,14 @@
       config.projectRoot = ./.;
       source = ./.;
       projects = ./projects.toml;
+      settings = [
+        # for all impure nodejs projects with just a `package.json`,
+        # add arguments for the `package-json` translator
+        {
+          filter = project: project.translator == "package-json";
+          subsystemInfo.npmArgs = "--legacy-peer-deps";
+          subsystemInfo.nodejs = 16;
+        }
+      ];
     };
 }
