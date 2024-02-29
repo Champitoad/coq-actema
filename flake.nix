e4cb28ae2509adfed6446fbf69701d74999d802c
Pablo Donato <pablo.donato@tuta.io>
2024-02-29 20:04:30 +0100

    System-independent nix flake for coq-actema

--- flake.nix
+++ flake.nix
@@ -1,36 +1,38 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
     prover.url = "./actema-desktop/prover";
   };
 
-  outputs = { self, nixpkgs, prover }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      ocpkgs = pkgs.ocaml-ng.ocamlPackages_4_14;
-      coq = pkgs.coq.override {
-        version = "8.15.2";
-        coq-version = "8.15.2";
-        customOCamlPackages = ocpkgs;
-      };
-      coqPackages = pkgs.mkCoqPackages coq;
+  outputs = { self, flake-utils, nixpkgs, prover }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        ocpkgs = pkgs.ocaml-ng.ocamlPackages_4_14;
+        coq = pkgs.coq.override {
+          version = "8.15.2";
+          coq-version = "8.15.2";
+          customOCamlPackages = ocpkgs;
+        };
+        coqPackages = pkgs.mkCoqPackages coq;
 
-      coq-actema = pkgs.callPackage ./coq-actema.nix {
-        inherit coqPackages;
-      };
+        coq-actema = pkgs.callPackage ./coq-actema.nix {
+          inherit coqPackages;
+        };
 
-      prover-pkg = prover.packages.x86_64-linux.default;
-    in
-    {
-      packages.x86_64-linux.default = coq-actema;
-      devShell.x86_64-linux = pkgs.mkShell {
-        inputsFrom = [ coq-actema ];
-        packages = [ coq ocpkgs.ocaml-lsp ];
-        APIDIR = "${prover-pkg}/lib/ocaml/4.14.1/site-lib/prover/api";
-      };
-      apps.x86_64-linux.vscoq-actema = {
-        type = "app";
-        program = "${pkgs.vscodium}/bin/codium";
-      };
-    };
+        prover-pkg = prover.packages.${system}.default;
+      in {
+        packages.default = coq-actema;
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ coq-actema ];
+          packages = [ coq ocpkgs.ocaml-lsp ];
+          APIDIR = "${prover-pkg}/lib/ocaml/4.14.1/site-lib/prover/api";
+        };
+        apps.vscoq-actema = {
+          type = "app";
+          program = "${pkgs.vscodium}/bin/codium";
+        };
+      }
+    );
 }
