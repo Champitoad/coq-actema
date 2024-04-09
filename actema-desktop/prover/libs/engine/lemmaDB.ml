f7ba7319ff96ac394f5eee4ed56f55b1a1aac717
MathisBD <mathis.bouverot@ens.psl.eu>
2024-04-09 16:16:00 +0200

    updated [build-prover] target to use nix stuff (in actema-desktop/package.json)

--- actema-desktop/prover/libs/engine/lemmaDB.ml
+++ actema-desktop/prover/libs/engine/lemmaDB.ml
@@ -4,6 +4,7 @@ type t = { db_env : Fo.env; db_map : (string, Fo.form) Map.t }
 
 exception LemmaNotFound of string
 
+
 let env db = db.db_env
 let get db name = Option.get_exn (Map.find_opt name db.db_map) (LemmaNotFound name)
 let empty env = { db_env = env; db_map = Map.empty }
