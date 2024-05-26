d8d224e167eec9d62b721a842ddcd23ff6e975ea
Pablo Donato <pablo.donato@tuta.io>
2024-05-26 13:45:39 +0200

    Go back to dream2nix legacy for frontend flake

--- frontend/flake.nix
+++ frontend/flake.nix
@@ -1,26 +1,32 @@
 {
-  inputs = {
-    nixpkgs.url = "nixpkgs";
-    old.url = "nixpkgs/nixos-23.05";
-  };
-  outputs = { self, nixpkgs, old }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      oldpkgs = import old {
-        system = "x86_64-linux";
-	config = {
-          permittedInsecurePackages = [
-	    "nodejs-14.21.3"
-	    "nodejs-16.20.2"
-	    "openssl-1.1.1w"
-          ];
-        };
-      };
-    in {
-    devShell.x86_64-linux = pkgs.mkShell {
-      packages = [
-        oldpkgs.nodejs_16
+  inputs.dream2nix.url = "github:nix-community/dream2nix/legacy";
+  outputs = { self, dream2nix }:
+    dream2nix.lib.makeFlakeOutputs {
+      systems = ["x86_64-linux"];
+      config.projectRoot = ./.;
+
+      source = ./.;
+
+      # `projects` can alternatively be an attrset.
+      # `projects` can be omitted if `autoProjects = true` is defined.
+      projects = ./projects.toml;
+      # autoProjects = true;
+
+      # Configure the behavior of dream2nix when translating projects.
+      # A setting applies to all discovered projects if `filter` is unset,
+      # or just to a subset or projects if `filter` is used.
+      settings = [
+        # prefer aggregated source fetching (large FODs)
+        {
+          aggregate = true;
+        }
+        # for all impure nodejs projects with just a `package.json`,
+        # add arguments for the `package-json` translator
+        {
+          # filter = project: project.translator == "package-json";
+          # subsystemInfo.npmArgs = "--legacy-peer-deps";
+          # subsystemInfo.nodejs = 16;
+        }
       ];
     };
-  };
 }
