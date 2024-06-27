09b5fc97317497c82a96c46d2dd0ac97ace91901
MathisBD <mathis.bouverot@ens.psl.eu>
2024-06-27 18:28:31 +0200

    better error behaviour when searching for lemmas with more than one selection

--- theories/Test.v
+++ theories/Test.v
@@ -2,6 +2,9 @@ From Actema Require Import Loader.
 Require Import ssreflect.
 
 
+
+
+
 Fixpoint eqb n m :=
   match n, m with
   | 0, 0 => true
@@ -9,29 +12,18 @@ Fixpoint eqb n m :=
   | _, _ => false
   end.
 
-
-Lemma test n m : eqb (n + m) m = true -> n = m.
-Proof. 
-actema_force.
-generalize m.
-  induction n.
-  actema_force.
-
 (* Here we can't instantiate [x]. *)
 Parameter (P : nat -> Prop).
 Lemma test a (b := a + a) : ~ forall x, exists y, P x /\ P y.
-actema_force.
+Admitted.
 
-(* Here we can't rewrite the second 0 into f0 with <-[f 0 = 0]. *)
-Parameter (f g : nat -> nat).
+(* Here we can't rewrite the first 0 into f0 with <-[f 0 = 0]. *)
+Parameter (f : nat -> nat).
+Parameter (Q : nat -> Prop).
 Lemma bug1 :
-   ~f (g 0) = 0 -> ((f 0) = 0 -> False).
+   Q 0 -> ~ (f 0) = 0.
 actema_force.
-
-(* Simplify proof after dnds. *)
-
-(* Simplify + try discriminate after case. *)
-
-(* Remember equality after case/induction on closed subterm (not on quantifier). *)
 
 (* Filter lemmas : only supports a single selection -> recover from this. *)
+
+(* Double click on hypothesis : try discriminate. *)
