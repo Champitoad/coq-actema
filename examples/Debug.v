f914f3b88e11881e150ceb328cd86dd659fed94a
MathisBD <mathis.bouverot@ens.psl.eu>
2024-11-24 19:34:29 +0100

    fix rewrite + instantiate dnds

--- examples/Debug.v
+++ examples/Debug.v
@@ -1,30 +1,6 @@
 From Actema Require Import Loader.
 Require Import ssreflect.
 
-Lemma bug (x y : nat) (h : x = y) : x = y.
-actema_force.  
-Qed.
-
-Parameter test : nat -> nat -> bool.
-Lemma bug2  (refl : forall n, test n n = true): 
- forall a, test a a = true.
- actema_force.
- intro.
- reflexivity.
- Unshelve.
-Qed.
-
-Parameter f g : nat -> nat.
-
-Lemma bug (x y : nat) (h : x = y) (e : x = f y) : x = y.
-actema_force.
-
-Lemma test2 (A B : Prop) (h : A -> B) : A -> B.
-Proof. actema_force.
-
-Parameter (Even : nat -> Prop).
-
-Lemma example a (h : forall n, Even n -> Even (n + 2)) : 
-  Even (a + 2).
-Proof. 
-  actema.
+Parameter P : nat -> Prop.
+Lemma bug3 (e : 4 = 4) : exists x, P x.
+actema.
